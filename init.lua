202b0a9b7b2675e39af89d48631e005204fe1ec8
serjkhon <149728454+serjkhon@users.noreply.github.com>
2026-05-24 17:49:32 -0400

    more flexibility in dependencies; updated init & forced lazyvim configs
    may take a few seconds to compile

--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.keymap.set('n', '\\', ':term python %<CR>')
+vim.keymap.set("n", "\\", ":term python %<CR>")
 
 vim.api.nvim_create_user_command("Lat", function()
 local dir = vim.fn.expand("%:p:h")
 local file = vim.fn.expand("%:p")
 local pdf = vim.fn.expand("%:p:r") .. ".pdf"
-vim.fn.jobstart("cd " .. dir .. " && latexmk -pdf " .. file .. " && zathura " .. pdf, {
-    detach = true,
-})
+
+local cmd = "cd " .. vim.fn.shellescape(dir)
+.. " && latexmk -pdf " .. vim.fn.shellescape(file)
+.. "; if [ -f " .. vim.fn.shellescape(pdf) .. " ]; then zathura "
+.. vim.fn.shellescape(pdf) .. " & fi"
+
+vim.fn.jobstart(cmd, { detach = true })
 end, {})
